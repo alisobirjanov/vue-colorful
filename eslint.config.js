0bbd1a9587891e18c8ce907f33a7362cae82a2a1
Alimardon <alisobirzanov29@gmail.com>
2024-01-13 11:26:57 +0500

    added eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,18 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-
+  vue: true,
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+  },
+  rules: {
+    'antfu/top-level-function': 'off',
+    'prefer-exponentiation-operator': 'off',
+    'unicorn/prefer-number-properties': 'off',
+    'ts/no-use-before-define': 'off',
+    'prefer-template': 'off',
+  },
 })
