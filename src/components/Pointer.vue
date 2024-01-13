07935483238ae7ea7d8620bc907e2481079933e8
Alimardon <alisobirzanov29@gmail.com>
2024-01-13 12:09:31 +0500

    feat: pointer component

---
+++ src/components/Pointer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div 
+    :class="nodeClassName" 
+    :style="{
+      top: `${top * 100}%`,
+      left: `${left * 100}%`,
+    }"
+  >
+    <div class="react-colorful__pointer-fill" :style="{ backgroundColor: color }" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { formatClassName } from '../utils'
+
+interface Props {
+  // className?: string;
+  top?: number;
+  left: number;
+  color: string;
+}
+
+const { top = 0 } = defineProps<Props>()
+
+const nodeClassName = formatClassName(["react-colorful__pointer", /* className */]);
+
+</script>
