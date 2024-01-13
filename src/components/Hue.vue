0494c5af13d1a5dad13bfac3567489cc80cb8e71
Alimardon <alisobirzanov29@gmail.com>
2024-01-13 14:19:02 +0500

    feat: Hue

---
+++ src/components/Hue.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { clamp, formatClassName, hsvaToHslString, round } from '../utils'
+import type { Interaction } from './Interactive.vue'
+import Interactive from './Interactive.vue'
+import Pointer from './Pointer.vue'
+
+interface Props {
+  hue: number
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<{
+  (e: 'onChange', newHue: { h: number }): void
+}>()
+
+const handleMove = (interaction: Interaction) => {
+  emit('onChange', { h: 360 * interaction.left })
+}
+
+const handleKey = (offset: Interaction) => {
+  // Hue measured in degrees of the color circle ranging from 0 to 360
+  emit('onChange', {
+    h: clamp(props.hue + offset.left * 360, 0, 360),
+  })
+}
+
+const nodeClassName = formatClassName(['react-colorful__hue'])
+</script>
+
+<template>
+  <div :className="nodeClassName">
+    <Interactive
+      aria-label="Hue" :aria-valuenow="round(hue)" aria-valuemax="360" aria-valuemin="0" @on-move="handleMove"
+      @on-key="handleKey"
+    >
+      <Pointer
+        class="react-colorful__hue-pointer" :left="hue / 360"
+        :color="hsvaToHslString({ h: hue, s: 100, v: 100, a: 1 })"
+      />
+    </Interactive>
+  </div>
+</template>
