cc8035da2a98db2a13c0b4a5e42fd878bf4cb962
Alimardon <alisobirzanov29@gmail.com>
2024-01-18 23:21:28 +0500

    Add lib mode config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-colorful',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
+
